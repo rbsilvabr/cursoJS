e809538db1bbeacf67b0b2c587d4dfb839a49878
Rodrigo Belo <rodrigobelosilva@gmail.com>
2020-02-07 12:04:41 -0300

    ok

--- aula14/src/controllers/homeController.js
+++ aula14/src/controllers/homeController.js
@@ -1,3 +1,17 @@
+// const HomeModel = require('../models/HomeModel');
+
+// HomeModel.create({
+//     titulo: 'Outra coisa',
+//     descricao: 'Outra descrição de testes.'
+// })
+// .then(dados => console.log(dados))
+// .catch(e => console.log(e));
+
+// HomeModel.find()
+// .then(dados => console.log(dados))
+// .catch(e => console.log(e));
+
+
 exports.paginaInicial = (req, res) => {
     console.log('Respondendo ao cliente.');
     //console.log(`Olha o que tem no req.session.nome: ${req.session.nome}`)
